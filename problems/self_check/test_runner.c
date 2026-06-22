60ca3b6ceb9dccea9f35df38d65d0b1876c9ff66
PineXmas <pinexmas@gmail.com>
2026-06-22 19:49:13 +0000

    Add more tests to self_check to make sure Testing panel works as expected

--- problems/self_check/test_runner.c
+++ problems/self_check/test_runner.c
@@ -6,7 +6,18 @@ void test_hello_world(void) {
     TEST_CHECK(1 == 1);
 }
 
+void test_failed(void) {
+    printf("\nThis test is designed to fail.\n");
+    TEST_CHECK(1 == 0);
+}
+
+void test_passed(void) {
+    TEST_CHECK(1 == 1);
+}
+
 TEST_LIST = {
     { "Verify Hello World UI Execution", test_hello_world },
+    { "Verify Failed Test Execution", test_failed },
+    { "Verify Passed Test Execution", test_passed },
     { NULL, NULL }
 };
